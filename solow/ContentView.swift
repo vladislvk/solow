1becd4a8ee5236b8cba07edae2ae58290a49cbe1
vladislvk <vladislavkim566@gmail.com>
2024-11-06 23:14:21 +0500

    feat:blank

--- solow/ContentView.swift
+++ solow/ContentView.swift
@@ -3,13 +3,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            homeScreen()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
         }
-        .padding()
     }
 }
 
