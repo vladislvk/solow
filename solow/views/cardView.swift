7a5ed9ed543408a7b4b00482f8964dcbaac9901e
vladislvk <vladislavkim566@gmail.com>
2024-11-08 17:31:01 +0500

    feat:blank

--- solow/views/cardView.swift
+++ solow/views/cardView.swift
@@ -1,11 +1,52 @@
 import SwiftUI
 
 struct cardView: View {
+    @State private var offset = CGSize.zero
+    @State private var isSwiped = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white
+                .frame(width: 300, height: 500)
+                .cornerRadius(20)
+                .shadow(radius: 10)
+                .overlay(
+                    Text("Swipe Me")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                )
+                .offset(x: offset.width, y: offset.height)
+                .rotationEffect(.degrees(Double(offset.width / 20)))
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            offset = gesture.translation
+                        }
+                        .onEnded { _ in
+                            if offset.width > 150 {
+                                offset = CGSize(width: 500, height: 0)
+                                isSwiped = true
+                            } else if offset.width < -150 {
+                                offset = CGSize(width: -500, height: 0)
+                                isSwiped = true
+                            } else {
+                                offset = .zero
+                            }
+                        }
+                )
+                .animation(.spring(), value: offset)
+        }
+        .onAppear {
+            if isSwiped {
+                offset = .zero
+                isSwiped = false
+            }
+        }
     }
 }
 
-#Preview {
-    cardView()
+struct SwipeCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        cardView()
+    }
 }
